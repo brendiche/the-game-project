6b5029c368be35f0ec4ab6a8d9d8f4a73e19eda4
brendiche <brendan.gouin92@gmail.com>
2022-02-06 22:25:57 +0100

    test: improve character test

--- src/character.spec.ts
+++ src/character.spec.ts
@@ -3,6 +3,9 @@
  */
 import { Character } from "./character";
 import { Engine } from "./gameEngine";
+const dateNowStub = () => 1644182219303;
+global.Date.now = dateNowStub;
+
 
 const mockGamingThreadCallback = jest.fn();
 const mockEngine: Partial<Engine> = {
@@ -17,9 +20,24 @@ describe('charater class', () => {
     expect(character.properties).toMatchSnapshot();
     expect(mockGamingThreadCallback).toHaveBeenCalled();
   });
-  it('should test the events', () => {
-    const arrowDownEvent = new KeyboardEvent('keydown', {key: 'ArrowDown'})
+  it('should test the KeyboardEvent', () => {
+    const arrowDownEvent = new KeyboardEvent('keydown', {key: 'ArrowDown'});
+    const arrowLeftEvent = new KeyboardEvent('keydown', {key: 'ArrowRight'});
+    const arrowRightEvent = new KeyboardEvent('keydown', {key: 'ArrowLeft'});
+    const arrowUpEvent = new KeyboardEvent('keydown', {key: 'ArrowUp'});
+    const spaceEvent = new KeyboardEvent('keydown', {key: ' '});
+    const sEvent = new KeyboardEvent('keydown', {key: 's'});
     window.dispatchEvent(arrowDownEvent);
-    expect(character.properties.state).toBe('down');
+    expect(character.properties).toMatchSnapshot();
+    window.dispatchEvent(arrowLeftEvent);
+    expect(character.properties).toMatchSnapshot();
+    window.dispatchEvent(arrowRightEvent);
+    expect(character.properties).toMatchSnapshot();
+    window.dispatchEvent(arrowUpEvent);
+    expect(character.properties).toMatchSnapshot();
+    window.dispatchEvent(spaceEvent);
+    expect(character.properties).toMatchSnapshot();
+    window.dispatchEvent(sEvent);
+    expect(character.properties).toMatchSnapshot();
   })
 })
